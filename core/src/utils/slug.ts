d4e4e1bba0697befddd6388f3ec053810d6966f5
bloodnighttw <bbeenn1227@gmail.com>
2025-03-03 20:26:31 +0800

    feat: slug function implement.

--- core/src/utils/slug.ts
+++ core/src/utils/slug.ts
@@ -4,12 +4,26 @@ export function removeExtension(filepath: string) {
     return path.parse(filepath).name;
 }
 
+const cyrb53 = (str: string, seed = 0) => {
+    let h1 = 0xdeadbeef ^ seed;
+    let h2 = 0x41c6ce57 ^ seed;
+    for(let i = 0, ch = null; i < str.length; i++) {
+        ch = str.charCodeAt(i);
+        h1 = Math.imul(h1 ^ ch, 2654435761);
+        h2 = Math.imul(h2 ^ ch, 1597334677);
+    }
+    h1  = Math.imul(h1 ^ (h1 >>> 16), 2246822507);
+    h1 ^= Math.imul(h2 ^ (h2 >>> 13), 3266489909);
+    h2  = Math.imul(h2 ^ (h2 >>> 16), 2246822507);
+    h2 ^= Math.imul(h1 ^ (h1 >>> 13), 3266489909);
+
+    return 4294967296 * (2097151 & h2) + (h1 >>> 0);
+};
+
 export function hashing(filepath: string) {
-    throw new Error("Not implemented");
-    return filepath;
+    return cyrb53(filepath).toString(36);
 }
 
 export function mix(filepath: string) {
-    throw new Error("Not implemented");
-    return filepath;
+    return `${removeExtension(filepath)}-${hashing(filepath)}`;
 }
