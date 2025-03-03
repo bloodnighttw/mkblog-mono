d103d0e77eedf2ce6f4bdb7f65060c73d08b21dc
bloodnighttw <bbeenn1227@gmail.com>
2025-03-03 23:24:07 +0800

    feat(vitest): use config.test file instead.

---
+++ vitest.config.ts
@@ -0,0 +1,23 @@
+import {defineConfig} from 'vitest/config'
+
+const exclude = [
+    '**/cypress/**',
+    '**/.{idea,git,cache,output,temp}/**',
+    "**/*.base.*",
+    "**/*.config.*",
+    '**/dist/**',
+    "**/coverage/**",
+    "**/node_modules/**",
+    "**/test/**",
+]
+
+export default defineConfig({
+    test: {
+        workspace: ['core'],
+        exclude,
+        coverage: {
+            provider: 'v8',
+            exclude
+        },
+    }
+})
