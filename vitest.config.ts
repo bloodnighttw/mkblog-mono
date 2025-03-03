9e792799190c612c1fd0bacc6d3c29c5c02a1171
bloodnighttw <bbeenn1227@gmail.com>
2025-03-04 00:33:03 +0800

    refactor(biome): reformat code using biome.

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,25 +1,25 @@
-import {defineConfig} from 'vitest/config'
+import { defineConfig } from "vitest/config";
 
 const exclude = [
-    '**/cypress/**',
-    '**/.{idea,git,cache,output,temp}/**',
-    "**/*.base.*",
-    "**/*.config.*",
-    '**/dist/**',
-    "**/coverage/**",
-    "**/node_modules/**",
-    "**/test/**",
-]
+	"**/cypress/**",
+	"**/.{idea,git,cache,output,temp}/**",
+	"**/*.base.*",
+	"**/*.config.*",
+	"**/dist/**",
+	"**/coverage/**",
+	"**/node_modules/**",
+	"**/test/**",
+];
 
 export default defineConfig({
-    test: {
-        workspace: ['core'],
-        exclude,
-        coverage: {
-            provider: 'v8',
-            exclude,
-            reporter: ['json', 'json-summary'],
-            reportOnFailure: true,
-        },
-    }
-})
+	test: {
+		workspace: ["core"],
+		exclude,
+		coverage: {
+			provider: "v8",
+			exclude,
+			reporter: ["json", "json-summary"],
+			reportOnFailure: true,
+		},
+	},
+});
