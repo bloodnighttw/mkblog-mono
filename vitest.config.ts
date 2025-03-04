dc93916e0997a344a5fc1b47263e679cae170935
bloodnighttw <bbeenn1227@gmail.com>
2025-03-05 01:11:58 +0800

    chore: add text report from coverage.

--- vitest.config.ts
+++ vitest.config.ts
@@ -18,7 +18,7 @@ export default defineConfig({
 		coverage: {
 			provider: "v8",
 			exclude,
-			reporter: ["json", "json-summary"],
+			reporter: ["json", "json-summary", "text"],
 			reportOnFailure: true,
 		},
 	},
