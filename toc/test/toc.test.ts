eebe0b3ddd6ebb3a75d2514611e5e7eb831bdde4
bloodnighttw <bbeenn1227@gmail.com>
2025-03-07 07:47:10 +0800

    test: new test to improve coverage.

--- toc/test/toc.test.ts
+++ toc/test/toc.test.ts
@@ -4,7 +4,7 @@ import {defineContent,zod} from "../../core/src";
 import remarkGfm from "remark-gfm";
 import {inspect} from "unist-util-inspect";
 
-test("toc", async ()=> {
+test("Table of content hast check", async ()=> {
 
     const config = await defineContent({
         include: ["./cases/toc/*.md"],
@@ -23,4 +23,24 @@ test("toc", async ()=> {
 
     expect(tocTree).toMatchSnapshot();
 
+})
+
+test("Table of content html string check", async ()=> {
+
+    const config = await defineContent({
+        include: ["./cases/toc/*.md"],
+        schema: {
+            title: zod.string(),
+        },
+        mkBlogPlugins: [
+            init
+        ],
+        remarkPlugins: [remarkGfm],
+    })
+
+    const post = config.posts.find((post) => post.slug === "toc");
+    // @ts-ignore mono-repo issue
+    const toc = await post.toc();
+
+    expect(toc).toMatchSnapshot();
 })
